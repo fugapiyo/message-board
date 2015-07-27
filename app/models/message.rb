0cd8e447f9dd15160db78e3d28360a2eb3addf0d
fugapiyo <amwtpgaj@softbank.ne.jp>
2015-07-27 14:30:47 +0000

    modify age validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,9 @@ class Message < ActiveRecord::Base
   # presence: true => 必須入力
   validates :name, length: {maximum: 20}, presence: true
   validates :body, length: {minimum: 2, maximum: 30 }, presence: true
-  validates :age,  numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }, allow_blank: true, presence: false
+  AGE_GE = 0
+  AGE_LE = 100
+  validates :age,  numericality: { only_integer: true, greater_than_or_equal_to: AGE_GE, less_than_or_equal_to: AGE_LE }, allow_blank: true, presence: false
 
   def get_age
     self.age ? self.age  : "年齢不詳"
